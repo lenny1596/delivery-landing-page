08692cfb2aa8fe4e1a7ef7168775a97b37f450e0
Lenin <leninbose1596@gmail.com>
2023-12-14 23:52:22 +0530

    added a box-shadow to navbar (scss) + added a scrollup btn (html) (scss) + added btn and navbar behv. in JS

--- js/app.js
+++ js/app.js
@@ -20,3 +20,48 @@ function linkAction() {
 }
 
 removeMenu.forEach((item) => item.addEventListener("click", linkAction));
+
+/* =============== SCROLL SECTIONS ACTIVE LINK ===============*/
+// const sections = document.querySelectorAll("section[id]");
+
+// function scrollActive() {
+//   const scrollY = window.scrollY;
+
+//   sections.forEach((current) => {
+//     const sectionHeight = current.offsetHeight;
+//     const sectionTop = current.offsetTop - 50;
+//     const sectionId = current.getAttribute("id");
+
+//     const link = document.querySelector(`.nav__menu li a[href*=${sectionId}]`);
+
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       link.classList.add("active-link");
+//     } else {
+//       link.classList.remove("active-link");
+//     }
+//   });
+// }
+// window.addEventListener("scroll", scrollActive);
+
+/* =============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (window.scrollY >= 50) {
+    header.classList.add("scroll-header");
+  } else {
+    header.classList.remove("scroll-header");
+  }
+}
+window.addEventListener("scroll", scrollHeader);
+
+/* =============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.querySelector(".scrollup__icon");
+  if (window.scrollY >= 1000) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUp.classList.remove("show-scroll");
+  }
+}
+window.addEventListener("scroll", scrollUp);
